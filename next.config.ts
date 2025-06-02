fc182e3694949f6b307eeb1845b8d3c889c444f2
Yusuf Kaya <yusufkaya380908@gmail.com>
2025-06-02 23:36:07 +0300

    Fix build script and convert next.config to JS format

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'standalone',
-  serverExternalPackages: ['sharp', 'canvas'],
-};
-
-export default nextConfig; 
