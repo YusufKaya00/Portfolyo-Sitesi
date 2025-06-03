2b8842600208d6aff9cdeda9b7441d20febb6413
Yusuf Kaya <yusufkaya380908@gmail.com>
2025-06-03 23:58:36 +0300

    next eklendi

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Disable TypeScript type checking during production build
+  typescript: {
+    // !! WARN !!
+    // This allows production builds to successfully complete even if
+    // your project has TypeScript errors
+    ignoreBuildErrors: true,
+  },
+  // Disable ESLint during production build
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig; 
